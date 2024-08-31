f60385a250372a6d62f01d27213e8b50c793da80
MinuraWije <minurawije@gmail.com>
2024-08-31 10:45:22 +0530

    first commit

---
+++ src/main/java/org/example/aadinclass/AadInclassApplication.java
@@ -0,0 +1,13 @@
+package org.example.aadinclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadInclassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AadInclassApplication.class, args);
+    }
+
+}
